996c48601c40abb0f36bfbc3d7139a94912e890a
root <rvpanoz@gmail.com>
2017-09-19 19:46:24 +0300

    add shell module to execute npm commands, add react main components, add config.js file, add main.css file

--- main.js
+++ main.js
@@ -4,26 +4,51 @@
 
 import {
   app,
-  BrowserWindow
+  BrowserWindow,
+  dialog
 } from 'electron';
 
-let mainWindow = null;
-let cwd = process.cwd();
-let NODE_ENV = process.env.NODE_ENV;
+const cwd = process.cwd();
+const NODE_ENV = process.env.NODE_ENV;
+const config = require('./config');
+const electronStore = require('electron-store');
+const shell = require('./shell');
+const winston = require('winston');
 
-// get parameters
+let platform = process.platform;
+let mainWindow = null;
 let debug = /--debug/.test(process.argv[4]);
+
+global.logger = new winston.Logger({
+  level: 'info',
+  transports: [
+    new(winston.transports.Console)(),
+    new(winston.transports.File)({
+      filename: 'log.log'
+    })
+  ]
+});
 
 //development mode
 if (NODE_ENV === 'development' && debug) {
   require('./development/imports.js');
 }
 
+//instatiate electron store
+const store = new electronStore();
+
+// set store and config as global objects
+// so we can call them via remote.getGlobal in a renderer process
+global.store = store;
+global.config = config;
+
 function createMainWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    show: true
   });
+
   mainWindow.loadURL('file://' + __dirname + '/index.html');
   mainWindow.on('closed', () => {
     mainWindow = null;
@@ -39,3 +64,16 @@ app.on('window-all-closed', () => {
 app.on('ready', () => {
   createMainWindow();
 });
+
+app.on('gpu-process-crashed', (event, killed) => {
+  throw new Error('gpu crached or is killed');
+})
+
+process.on('uncaughtException', function(err) {
+  console.log(err);
+});
+
+// GPU AMD fix for Linux
+if(platform === 'linux') {
+  app.commandLine.appendSwitch('disable-gpu-compositing');
+}
