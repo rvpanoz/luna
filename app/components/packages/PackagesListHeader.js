4e567f84f26e9525e611790b80b31ff5d90b34f0
rvpanoz <rvpanoz@gmail.com>
2017-11-03 10:21:21 +0200

    css fixes

--- app/components/packages/PackagesListHeader.js
+++ app/components/packages/PackagesListHeader.js
@@ -22,34 +22,36 @@ class PackagesListHeader extends React.Component {
   render() {
     let props = this.props;
     return (
-      <div className={styles.packages__head}>
-        <div className={styles.packages__title}>
-          <span>Packages</span>&nbsp;
-          <span className="label" style={{
-            display: 'inline-block',
-            minWidth: '45px',
-            color: '#fff'
-          }}>{props.total}</span>
+      <div style={{marginBottom:'15px'}}>
+        <div className={styles.packages__head}>
+          <div className={styles.packages__title}>
+            <span>Packages&nbsp;</span>
+            <span className="label" style={{
+              display: 'inline-block',
+              minWidth: '45px',
+              color: '#fff'
+            }}>{props.total}</span>
+          </div>
+          <div className={styles.packages__actions}>
+            <div className={styles.packages__action}>
+              <i className="fa fa-fw fa-refresh" onClick={this._fetch} title="Reload"></i>
+            </div>
+            <div className={`${styles.packages__action} dropdown`}>
+              <i className="fa fa-fw fa-cog dropdown-toggle" data-toggle="dropdown"></i>
+              <ul className="dropdown-menu dropdown-menu-right">
+                <li className="dropdown-header">Actions</li>
+                <li>
+                  <a href="#" onClick={this._setGlobalMode}>
+                    <i className="fa fa-fw fa-reply"></i>
+                    <span>Switch to global mode</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-        <div className={styles.packages__actions}>
-          <div className={styles.packages__action}>
-            <span className="label label-info">{props.mode}</span>
-          </div>
-          <div className={styles.packages__action}>
-            <i className="fa fa-fw fa-refresh" onClick={this._fetch} title="Reload"></i>
-          </div>
-          <div className={`${styles.packages__action} dropdown`}>
-            <i className="fa fa-fw fa-cog dropdown-toggle" data-toggle="dropdown"></i>
-            <ul className="dropdown-menu dropdown-menu-right">
-              <li className="dropdown-header">Actions</li>
-              <li>
-                <a href="#" onClick={this._setGlobalMode}>
-                  <i className="fa fa-fw fa-reply"></i>
-                  <span>Switch to global mode</span>
-                </a>
-              </li>
-            </ul>
-          </div>
+        <div className={styles.packages__mode}>
+          <span className="label label-info mode">{props.mode}</span>
         </div>
       </div>
     )
