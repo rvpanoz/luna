3504248191d7d7d8a55c7c84014750872bf701f9
rvpanos <rvpanoz@gmail.com>
2018-01-18 22:14:20 +0200

    prettier

--- app/reducers/packagesReducer.js
+++ app/reducers/packagesReducer.js
@@ -3,32 +3,31 @@
  Handles state management for packages operations
  **/
 
-"use strict";
+'use strict'
 
-import initialState from "./initialState";
-import * as R from "ramda";
+import initialState from './initialState'
+import * as R from 'ramda'
 
 import {
-  TOGGLE_MAIN_LOADER,
-  SET_PACKAGES,
-  TOTAL_INSTALLED,
-  SET_ACTIVE,
-  SET_PACKAGES_OUTDATED,
-  SET_PACKAGE_ACTIONS
-} from "../constants/ActionTypes";
+	TOGGLE_MAIN_LOADER,
+	SET_PACKAGES,
+	TOTAL_INSTALLED,
+	SET_ACTIVE,
+	SET_PACKAGES_OUTDATED,
+	SET_PACKAGE_ACTIONS
+} from '../constants/ActionTypes'
 
 //currying
 const createReducer = (initialState, handlers) => (state = initialState, action) =>
-  R.propOr(R.identity, R.prop("type", action), handlers)(state, action);
+	R.propOr(R.identity, R.prop('type', action), handlers)(state, action)
 
 const reducer = createReducer(initialState.packages, {
-  [SET_PACKAGES]: (state, action) => R.assoc("packages", action.packages, state),
-  [SET_PACKAGES_OUTDATED]: (state, action) => R.assoc("outdated", action.outdated, state),
-  [SET_ACTIVE]: (state, action) => R.assoc("active", action.active, state),
-  [TOGGLE_MAIN_LOADER]: (state, action) => R.assoc("isLoading", action.isLoading, state),
-  [SET_PACKAGE_ACTIONS]: (state, action) =>
-    R.assoc("actions", action.actions || state.actions, state),
-  [TOTAL_INSTALLED]: (state, action) => R.assoc("total", action.total, state)
-});
+	[SET_PACKAGES]: (state, action) => R.assoc('packages', action.packages, state),
+	[SET_PACKAGES_OUTDATED]: (state, action) => R.assoc('outdated', action.outdated, state),
+	[SET_ACTIVE]: (state, action) => R.assoc('active', action.active, state),
+	[TOGGLE_MAIN_LOADER]: (state, action) => R.assoc('isLoading', action.isLoading, state),
+	[SET_PACKAGE_ACTIONS]: (state, action) => R.assoc('actions', action.actions || state.actions, state),
+	[TOTAL_INSTALLED]: (state, action) => R.assoc('total', action.total, state)
+})
 
-export default reducer;
+export default reducer
