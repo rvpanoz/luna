a227ca9294f37c61860e6af8e6c05feeecf4cbbc
rvpanoz <rvpanoz@gmail.com>
2018-01-23 10:58:08 +0200

    WIP Layout

--- app/containers/Layout.js
+++ app/containers/Layout.js
@@ -2,69 +2,72 @@
  * Base Layout
  */
 
-'use strict'
+"use strict";
 
-import React from 'react'
-import PropTypes from 'prop-types'
-import * as R from 'ramda'
-import Grid from 'material-ui/Grid'
-import Paper from 'material-ui/Paper'
-import { withStyles } from 'material-ui/styles'
+import React from "react";
+import PropTypes from "prop-types";
+import * as R from "ramda";
+import Grid from "material-ui/Grid";
+import Paper from "material-ui/Paper";
+import { withStyles } from "material-ui/styles";
 
 const styles = (theme) => {
-	console.log('theme', theme)
-	return {
-		root: {
-			display: 'flex',
-			flexGrow: 1,
-			flexDirection: 'column'
-		}
-	}
-}
+  return {
+    root: {
+      display: "flex",
+      flexGrow: 1,
+      flexDirection: "column"
+    }
+  };
+};
 
 class Layout extends React.Component {
-	constructor(props) {
-		super(props)
-		this.AppHeader = null
-		this.AppMain = null
-		this.getElementByPosition = this.getElementByPosition.bind(this)
-	}
-	getElementByPosition(position) {
-		const { children } = this.props
-		return R.filter(
-			R.where({
-				props: R.propEq('position', position)
-			})
-		)(children)[0]
-	}
-	componentWillMount() {
-		this.AppHeader = this.getElementByPosition('header')
-		this.AppSidebar = this.getElementByPosition('sidebar')
-		this.AppMain = this.getElementByPosition('main')
-	}
-	render() {
-		const { classes } = this.props
+  static AppHeader = null;
+  constructor(props) {
+    super(props);
+    this.getElementByPosition = this.getElementByPosition.bind(this);
+  }
+  getElementByPosition(position) {
+    const { children } = this.props;
+    const isChildrenArray = Array.isArray(children);
 
-		return (
-			<div className={classes.root}>
-				<Grid container spacing={24}>
-					<Grid item xs={12} sm={12}>
-						<Paper className={classes.paper}>{this.AppHeader}</Paper>
-					</Grid>
-					<Grid item xs={12} sm={3}>
-						<Paper className={classes.paper}>{this.AppSidebar}</Paper>
-					</Grid>
-					<Grid item xs={12} sm={9}>
-						<Paper className={classes.paper}>{this.AppMain}</Paper>
-					</Grid>
-				</Grid>
-			</div>
-		)
-	}
+    if (isChildrenArray) {
+      return R.filter(
+        R.where({
+          props: R.propEq("position", position)
+        }),
+        children
+      )[0];
+    } else {
+      return children;
+    }
+  }
+  componentWillMount() {
+    this.AppHeader = this.getElementByPosition("header");
+  }
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <Grid container spacing={24}>
+          <Grid item xs={12} sm={12}>
+            <Paper className={classes.paper}>{this.AppHeader}</Paper>
+          </Grid>
+          <Grid item xs={12} sm={3}>
+            <Paper className={classes.paper} />
+          </Grid>
+          <Grid item xs={12} sm={9}>
+            <Paper className={classes.paper} />
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 Layout.propTypes = {
-	children: PropTypes.node.isRequired
-}
+  children: PropTypes.node.isRequired
+};
 
-export default withStyles(styles)(Layout)
+export default withStyles(styles)(Layout);
--- app/containers/Root.js
+++ app/containers/Root.js
@@ -2,45 +2,41 @@
  * App Component
  */
 
-'use strict'
+"use strict";
 
 //imports
-import React from 'react'
-import { Provider } from 'react-redux'
-import PropTypes from 'prop-types'
-import Reboot from 'material-ui/Reboot'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import createMuiTheme from 'material-ui/styles/createMuiTheme'
-import { APP_GLOBALS } from '../constants/AppConstants'
+import React from "react";
+import { Provider } from "react-redux";
+import PropTypes from "prop-types";
+import Reboot from "material-ui/Reboot";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import createMuiTheme from "material-ui/styles/createMuiTheme";
+import { APP_GLOBALS } from "../constants/AppConstants";
 
 //Components
-import Layout from './Layout'
-import Header from '../components/Header'
-import Sidebar from './Sidebar'
-import Main from './Main'
+import Layout from "./Layout";
+import Header from "../components/Header";
+import Sidebar from "./Sidebar";
+import Main from "./Main";
 
 export default function App(props) {
-	const { store } = props
+  const { store } = props;
 
-	//create material-ui custom theme
-	const muiTheme = createMuiTheme()
+  //create material-ui custom theme
+  const muiTheme = createMuiTheme();
 
-	return (
-		<section>
-			<Reboot />
-			<Provider store={store}>
-				<MuiThemeProvider theme={muiTheme}>
-					<Layout>
-						<section className="header">
-							<Header position="header" title={APP_GLOBALS.title} />
-						</section>
-						<section className="main">
-							<Sidebar position="sidebar" />
-							<Main position="main" />
-						</section>
-					</Layout>
-				</MuiThemeProvider>
-			</Provider>
-		</section>
-	)
+  return (
+    <section>
+      <Reboot />
+      <Provider store={store}>
+        <MuiThemeProvider theme={muiTheme}>
+          <Layout>
+            <Header position="header" title={APP_GLOBALS.title} />
+            <Sidebar position="sidebar" />
+            <Main position="main" />
+          </Layout>
+        </MuiThemeProvider>
+      </Provider>
+    </section>
+  );
 }
