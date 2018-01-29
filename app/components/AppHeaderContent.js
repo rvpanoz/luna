92c96b6c39c87c5634edcb6823373756d82c6911
rvpanoz <rvpanoz@gmail.com>
2018-01-29 17:10:28 +0200

    WIP - packages, settings (use modal)

--- app/components/AppHeaderContent.js
+++ app/components/AppHeaderContent.js
@@ -6,8 +6,10 @@ import List, { ListItem, ListItemIcon, ListItemText } from "material-ui/List";
 import Icon from "material-ui/Icon";
 import { APP_MODES } from "../constants/AppConstants";
 import { withStyles } from "material-ui/styles";
-import pink from "material-ui/colors/pink";
 import { appHeaderContentStyles } from "./styles";
+import Modal from "material-ui/Modal";
+import TextField from "material-ui/TextField";
+import Typography from "material-ui/Typography";
 
 class AppHeaderContent extends React.Component {
   constructor() {
@@ -17,6 +19,30 @@ class AppHeaderContent extends React.Component {
   }
   updateMode(directory) {
     ipcRenderer.send("analyze-json", directory);
+  }
+  getModalStyles() {
+    function rand() {
+      return Math.floor(Math.random() * 20) - 10;
+    }
+
+    const top = 50 + rand();
+    const left = 50 + rand();
+
+    return {
+      position: "absolute",
+      width: 8 * 50,
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+      border: "1px solid #e5e5e5",
+      backgroundColor: "#fff",
+      boxShadow: "0 5px 15px rgba(0, 0, 0, .5)",
+      padding: 8 * 4
+    };
+  }
+  openSettings() {}
+  handleChange(e) {
+    // console.log(e.target.value);
   }
   openPackage(e) {
     e.preventDefault();
@@ -40,31 +66,61 @@ class AppHeaderContent extends React.Component {
       }
     );
   }
-
   render() {
     const { classes } = this.props;
+    const npmRegistry = "https://registry.npmjs.org/";
+    const npmProxy = "http://proxy.company.com:8080";
 
     return (
-      <List>
-        <ListItem button onClick={this.openPackage}>
-          <ListItemIcon>
-            <Icon className={classes.iconHover}>send</Icon>
-          </ListItemIcon>
-          <ListItemText primary="Analyze" secondary="Open package.json" />
-        </ListItem>
-        <ListItem button>
-          <ListItemIcon>
-            <Icon>list</Icon>
-          </ListItemIcon>
-          <ListItemText primary="Outdated" secondary="Outdated packages" />
-        </ListItem>
-        <ListItem button>
-          <ListItemIcon>
-            <Icon>settings</Icon>
-          </ListItemIcon>
-          <ListItemText primary="Settings" secondary="Application settings" />
-        </ListItem>
-      </List>
+      <section>
+        <List>
+          <ListItem button onClick={this.openPackage}>
+            <ListItemIcon>
+              <Icon className={classes.iconHover}>send</Icon>
+            </ListItemIcon>
+            <ListItemText primary="Analyze" secondary="Open package.json" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <Icon>list</Icon>
+            </ListItemIcon>
+            <ListItemText primary="Outdated" secondary="Outdated packages" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <Icon>settings</Icon>
+            </ListItemIcon>
+            <ListItemText primary="Settings" secondary="Application settings" />
+          </ListItem>
+        </List>
+        <Modal
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+          open={true}
+          onClose={this.handleModal}
+        >
+          <div style={this.getModalStyles()} className={classes.paper}>
+            <form className={classes.container} noValidate autoComplete="off">
+              <TextField
+                id="npm-registry"
+                label="Registry"
+                className={classes.textField}
+                value={npmRegistry}
+                onChange={this.handleChange}
+                margin="normal"
+              />
+              <TextField
+                id="npm-proxy"
+                label="Proxy"
+                className={classes.textField}
+                value={npmProxy}
+                onChange={this.handleChange}
+                margin="normal"
+              />
+            </form>
+          </div>
+        </Modal>
+      </section>
     );
   }
 }
