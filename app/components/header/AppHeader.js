cfce4f645235f256eaeee4f812e0a041d2f65d0b
rvpanoz <rvpanoz@gmail.com>
2018-02-03 13:15:29 +0200

    fixes

--- app/components/header/AppHeader.js
+++ app/components/header/AppHeader.js
@@ -24,88 +24,116 @@ import ChevronRightIcon from 'material-ui-icons/ChevronRight'
 import AppHeaderContent from './AppHeaderContent'
 import SearchBox from './SearchBox'
 import { firstToUpper } from '../../utils'
-
 import { appHeaderStyles } from '../styles'
 
-class AppHeader extends React.Component {
-	constructor() {
-		super()
-	}
-	render() {
-		const {
-			menuOpen,
-			classes,
-			handleDrawerOpen,
-			handleDrawerClose,
-			mode,
-			directory,
-			theme,
-			toggleLoader,
-			toggleSettings
-		} = this.props
+const {
+	object
+} = PropTypes
 
-		return (
-			<section>
-				<AppBar className={classNames(classes.appBar, menuOpen && classes.appBarShift)}>
-					<div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-						<Toolbar disableGutters={!menuOpen}>
-							<IconButton
-								color="inherit"
-								aria-label="open menu"
-								onClick={handleDrawerOpen}
-								className={classNames(classes.menuButton, menuOpen && classes.hide)}
-							>
-								<MenuIcon />
-							</IconButton>
-						</Toolbar>
-						<div className={classes.info}>
-							<Icon className={classes.modeIcon}>{mode === APP_MODES.GLOBAL ? 'language' : 'home'}</Icon>
-							<span className={classes.mode}>{firstToUpper(mode)}</span>
-						</div>
-						<SearchBox toggleLoader={toggleLoader} />
-					</div>
-				</AppBar>
-				<Drawer
-					type="permanent"
-					classes={{
-						paper: classNames(classes.drawerPaper, !menuOpen && classes.drawerPaperClose)
-					}}
-					open={menuOpen}
-				>
-					<div className={classes.drawerInner}>
-						<div className={classes.drawerHeader}>
-							<IconButton onClick={handleDrawerClose}>
-								{theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-							</IconButton>
-						</div>
-						<Divider />
-						<AppHeaderContent toggleSettings={toggleSettings} />
-					</div>
-				</Drawer>
-			</section>
-		)
-	}
+class AppHeader extends React.Component {
+  constructor() {
+    super()
+  }
+  render() {
+    const {
+      menuOpen,
+      classes,
+      handleDrawerOpen,
+      handleDrawerClose,
+      mode,
+      directory,
+      theme,
+      toggleLoader,
+      toggleSettings
+    } = this.props
+
+    return (
+      <section>
+        <AppBar
+          className={classNames(
+            classes.appBar,
+            menuOpen && classes.appBarShift
+          )}
+        >
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between'
+            }}
+          >
+            <Toolbar disableGutters={!menuOpen}>
+              <IconButton
+                color="inherit"
+                aria-label="open menu"
+                onClick={handleDrawerOpen}
+                className={classNames(
+                  classes.menuButton,
+                  menuOpen && classes.hide
+                )}
+              >
+                <MenuIcon />
+              </IconButton>
+            </Toolbar>
+            <div className={classes.info}>
+              <Icon className={classes.modeIcon}>
+                {mode === APP_MODES.GLOBAL ? 'language' : 'home'}
+              </Icon>
+              <span className={classes.mode}>{firstToUpper(mode)}</span>
+            </div>
+            <SearchBox toggleLoader={toggleLoader} />
+          </div>
+        </AppBar>
+        <Drawer
+          type="permanent"
+          classes={{
+            paper: classNames(
+              classes.drawerPaper,
+              !menuOpen && classes.drawerPaperClose
+            )
+          }}
+          open={menuOpen}
+        >
+          <div className={classes.drawerInner}>
+            <div className={classes.drawerHeader}>
+              <IconButton onClick={handleDrawerClose}>
+                {theme.direction === 'rtl' ? (
+                  <ChevronRightIcon />
+                ) : (
+                  <ChevronLeftIcon />
+                )}
+              </IconButton>
+            </div>
+            <Divider />
+            <AppHeaderContent toggleSettings={toggleSettings} />
+          </div>
+        </Drawer>
+      </section>
+    )
+  }
 }
 
 function mapStateToProps(state) {
-	return {
-		mode: state.global.mode,
-		directory: state.global.directory
-	}
+  return {
+    mode: state.global.mode,
+    directory: state.global.directory
+  }
 }
 
 function mapDispatchToProps(dispatch) {
-	return {
-		toggleSettings: (bool) => dispatch(toggleSettings(bool)),
-		toggleLoader: (bool) => dispatch(toggleLoader(bool))
-	}
+  return {
+    toggleSettings: (bool) => dispatch(toggleSettings(bool)),
+    toggleLoader: (bool) => dispatch(toggleLoader(bool))
+  }
 }
 
 AppHeader.propTypes = {
-	classes: PropTypes.object.isRequired,
-	theme: PropTypes.object.isRequired
+  classes: object.isRequired,
+  theme: object.isRequired,
+	menuOpen:
 }
 
-export default compose(withStyles(appHeaderStyles, { withTheme: true }), connect(mapStateToProps, mapDispatchToProps))(
-	AppHeader
-)
+export default compose(
+  withStyles(appHeaderStyles, { withTheme: true }),
+  connect(mapStateToProps, mapDispatchToProps)
+)(AppHeader)
