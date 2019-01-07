2bee3d2faa4d640494e73503c0540d32e6d631d1
rvpanoz <rvpanoz@gmail.com>
2019-01-07 02:14:55 +0200

    fix git

--- app/models/ui/selectors.js
+++ app/models/ui/selectors.js
@@ -5,43 +5,19 @@ import {
   setSnackbar,
   setPage,
   setPageRows,
-<<<<<<< c95bb4b50524e27e2c0538331b8ba3eee2323202
-<<<<<<< 1beff8f651b44fbd14f7b672e915efa17d384a08
   toggleLoader,
   togglePackageLoader
-=======
-  toggleLoader
->>>>>>> clean up
-=======
-  toggleLoader,
-  togglePackageLoader
->>>>>>> Package details first implementation, add more selectors and clean up packages
 } from './actions';
 
 const doClearNotifications = dispatch => dispatch(clearNotifications());
 const doClearSnackbar = dispatch => dispatch(clearSnackbar());
 const doToggleLoader = (dispatch, payload) => dispatch(toggleLoader(payload));
-<<<<<<< c95bb4b50524e27e2c0538331b8ba3eee2323202
-<<<<<<< 1beff8f651b44fbd14f7b672e915efa17d384a08
 const doTogglePackageLoader = (dispatch, payload) =>
   dispatch(togglePackageLoader(payload));
-=======
->>>>>>> clean up
-=======
-const doTogglePackageLoader = (dispatch, payload) =>
-  dispatch(togglePackageLoader(payload));
->>>>>>> Package details first implementation, add more selectors and clean up packages
 const doSetPage = (dispatch, payload) => dispatch(setPage(payload));
 const doAddNotification = (dispatch, payload) =>
   dispatch(addNotification(payload));
 const doSetSnackbar = (dispatch, payload) => dispatch(setSnackbar(payload));
-<<<<<<< c95bb4b50524e27e2c0538331b8ba3eee2323202
-<<<<<<< 1beff8f651b44fbd14f7b672e915efa17d384a08
-=======
-
->>>>>>> clean up
-=======
->>>>>>> Package details first implementation, add more selectors and clean up packages
 const doSetPageRows = (dispatch, payload) => dispatch(setPageRows(payload));
 
 export {
@@ -49,14 +25,7 @@ export {
   doClearNotifications,
   doClearSnackbar,
   doToggleLoader,
-<<<<<<< c95bb4b50524e27e2c0538331b8ba3eee2323202
-<<<<<<< 1beff8f651b44fbd14f7b672e915efa17d384a08
   doTogglePackageLoader,
-=======
->>>>>>> clean up
-=======
-  doTogglePackageLoader,
->>>>>>> Package details first implementation, add more selectors and clean up packages
   doSetPage,
   doSetPageRows,
   doSetSnackbar
