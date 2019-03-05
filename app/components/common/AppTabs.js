ce25b38edfbcb8309d25d6a7f57a63ab9085b62a
Arvanitis Panagiotis <panagiotis.arvanitis@agileactors.com>
2019-03-05 16:58:59 +0200

    AppTabs refactor

--- app/components/common/AppTabs.js
+++ app/components/common/AppTabs.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/require-default-props */
 
 import path from 'path';
-import React, { useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
@@ -13,50 +13,37 @@ import Tab from '@material-ui/core/Tab';
 
 import styles from './styles/appTabs';
 
-function TabContainer({ children, dir }) {
-  return (
-    <Typography component="div" style={{ padding: 8 * 3 }}>
-      {children}
-    </Typography>
-  );
-}
+const TabContainer = ({ children, dir }) => (
+  <Typography component="div" style={{ padding: 8 * 2 }}>
+    {children}
+  </Typography>
+);
 
 TabContainer.propTypes = {
   children: PropTypes.node.isRequired
 };
 
-class AppTabs extends React.Component {
-  state = {
-    value: 0
-  };
+const AppTabs = ({ classes }) => {
+  const [value, setValue] = useState(0);
 
-  handleChange = (event, value) => {
-    this.setState({ value });
-  };
-
-  render() {
-    const { classes } = this.props;
-    const { value } = this.state;
-
-    return (
-      <div className={classes.root}>
-        <AppBar position="static" color="default">
-          <Tabs
-            value={this.state.value}
-            onChange={this.handleChange}
-            indicatorColor="primary"
-            textColor="primary"
-          >
-            <Tab label="Project" />
-            <Tab label="Packages" />
-          </Tabs>
-        </AppBar>
-        {value === 0 && <TabContainer>project info..</TabContainer>}
-        {value === 1 && <TabContainer>packages stats..</TabContainer>}
-      </div>
-    );
-  }
-}
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Tabs
+          value={value}
+          onChange={(e, value) => setValue(value)}
+          indicatorColor="primary"
+          textColor="primary"
+        >
+          <Tab label="Project" />
+          <Tab label="Packages" />
+        </Tabs>
+      </AppBar>
+      {value === 0 && <TabContainer>project info..</TabContainer>}
+      {value === 1 && <TabContainer>packages stats..</TabContainer>}
+    </div>
+  );
+};
 
 AppTabs.propTypes = {
   classes: PropTypes.object.isRequired,
