a22573c2354ca5dcd19ec8890167de428db0a04c
rvpanoz <rvpanoz@gmail.com>
2019-03-30 18:41:06 +0200

    fix filters issue

--- app/components/pages/packages/TableFilters.js
+++ app/components/pages/packages/TableFilters.js
@@ -77,7 +77,16 @@ const TableFilters = ({ classes, mode, close, listFilters }) => {
   );
 
   const handleFilters = allFilters => {
-    dispatch(updateFilters({ allFilters }));
+    const hasFilters =
+      allFilters.length === 1
+        ? allFilters.every(({ filterValue }) => filterValue !== undefined)
+        : true;
+
+    console.log(allFilters);
+    if (hasFilters) {
+      dispatch(updateFilters({ allFilters }));
+    }
+
     close();
   };
 
