aebe9a5dd4cb33e96065a7e7d4edff6e953b14aa
rvpanoz <rvpanoz@gmail.com>
2019-04-06 11:59:47 +0300

    create notification model

---
+++ app/models/notifications/actions.js
@@ -0,0 +1,9 @@
+import { createActionCreator } from 'commons/utils';
+
+const ActionCreator = createActionCreator('@@LUNA_APP/UI');
+
+const updateNotifications = ActionCreator('UPDATE_NOTIFICATIONS');
+const addNotification = ActionCreator('ADD_NOTIFICATION');
+const clearNotifications = ActionCreator('CLEAR_NOTIFICATIONS');
+
+export { updateNotifications, addNotification, clearNotifications };
