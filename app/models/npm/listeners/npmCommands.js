93b9539ce283c6022ab918e4abbab1d7ac2833a1
rvpanos <rvpanoz@gmail.com>
2019-04-24 16:53:10 +0300

    lint

--- app/models/npm/listeners/npmCommands.js
+++ app/models/npm/listeners/npmCommands.js
@@ -1,6 +1,5 @@
 import { ipcRenderer } from 'electron';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
 
 const onNpmCommand$ = new Observable(observer => {
   ipcRenderer.removeAllListeners(['on-command-flow']);
