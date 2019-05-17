b5f760ffce29648f03429fab31a2a6eed763ca57
rvpanoz <rvpanoz@gmail.com>
2019-05-18 00:12:06 +0300

    wip

--- app/mainProcess/index.js
+++ app/mainProcess/index.js
@@ -2,5 +2,6 @@ import onNpmView from './npmView';
 import onNpmSearch from './npmSearch';
 import onNpmListOutdated from './npmListOutdated';
 import onNpmInstall from './npmInstall';
+import onNpmUninstall from './npmUninstall';
 
-export { onNpmView, onNpmSearch, onNpmListOutdated, onNpmInstall };
+export { onNpmView, onNpmSearch, onNpmListOutdated, onNpmInstall, onNpmUninstall };
