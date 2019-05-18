b21bb306de1fa3b21802410003c4a2160b97885b
rvpanoz <rvpanoz@gmail.com>
2019-05-18 21:08:41 +0300

    wip

--- app/mainProcess/npmUninstall.js
+++ app/mainProcess/npmUninstall.js
@@ -9,39 +9,42 @@ const { defaultSettings } = config || {};
 const { defaultManager } = defaultSettings;
 
 const onNpmUninstall = (event, options, store) => {
-    const settings = store.get('user_settings');
-    const { activeManager = defaultManager, ...rest } = options || {};
+  const settings = store.get('user_settings');
+  const { activeManager = defaultManager, ...rest } = options || {};
 
-    const onFlow = chunk => event.sender.send('npm-uninstall-flow', chunk);
-    const onError = error => event.sender.send('npm-uninstall-error', error);
-    const onComplete = (errors, result, removedPackages) => event.sender.send('npm-uninstall-completed', result, errors, removedPackages);
+  const onFlow = chunk => event.sender.send('npm-uninstall-flow', chunk);
+  const onError = error => event.sender.send('npm-uninstall-error', error);
+  const onComplete = (errors, result, removedPackages) =>
+    event.sender.send(
+      'npm-uninstall-completed',
+      result,
+      errors,
+      removedPackages
+    );
 
-    const callback = result => {
-        const { status, errors, data } = result;
-        const { packages } = options;
+  const callback = result => {
+    const { status, errors, data } = result;
+    const { packages } = options;
 
-        return switchcase({
-            flow: dataChunk => onFlow(dataChunk),
-            close: () => onComplete(errors, data, packages),
-            error: error => onError(error)
-        })(null)(status);
-    };
+    return switchcase({
+      flow: dataChunk => onFlow(dataChunk),
+      close: () => onComplete(errors, data, packages),
+      error: error => onError(error)
+    })(null)(status);
+  };
 
-    /**
-     * At this point we try to run a shell command sending output
-     * to renderer process via ipc events
-     */
-    try {
-        const params = merge(settings, {
-            activeManager,
-            ...rest
-        });
+  // run npm uninstall and send the result to renderer process via ipc event
+  try {
+    const params = merge(settings, {
+      activeManager,
+      ...rest
+    });
 
-        runCommand(params, callback);
-    } catch (error) {
-        log.error(error.message);
-        event.sender.send('npm-uninstall-error', error);
-    }
+    runCommand(params, callback);
+  } catch (error) {
+    log.error(error.message);
+    event.sender.send('npm-uninstall-error', error);
+  }
 };
 
 export default onNpmUninstall;
