c822805589f057a3ab594e5b5cdb1b820d0a02b3
rvpanoz <rvpanoz@gmail.com>
2019-06-20 21:47:03 +0300

    Feature/app messages (#311)
    * load directory and History tabs (#310)
    
    * feat: iMessage, interpolate AppMessages
    
    * wip: continue work on iMessage, change yarn.lock event name in main-process
    
    * chore: update app message and use iMessage utility to interpolate
    
    * feat: iMessage, interpolate AppMessages
    
    * wip: continue work on iMessage, change yarn.lock event name in main-process
    
    * chore: update app message and use iMessage utility to interpolate

--- app/constants/AppMessages.js
+++ app/constants/AppMessages.js
@@ -70,6 +70,7 @@ export const TITLE_MESSAGES = {
 };
 
 export const WARNING_MESSAGES = {
+  notGlobalModeAvailable: 'Not available in global mode',
   peerMissing:
     'You have %packages% with peer missing. Check your notifications to fix it.',
   errorPackages:
