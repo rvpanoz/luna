baffa8ff11985af2b7080bf13678be9b242db0cb
rvpanoz <rvpanoz@gmail.com>
2019-06-27 06:31:23 +0300

    chore: fix imports and add lint rules to off

--- app/constants/AppMessages.js
+++ app/constants/AppMessages.js
@@ -118,6 +118,7 @@ export const LABEL_MESSAGE = {
 };
 
 export const TITLE_MESSAGES = {
+  system: 'Preview system',
   switchGlobals: 'Switch to global packages',
   showFilters: 'Show filters',
   installSelected: 'Install selected',
