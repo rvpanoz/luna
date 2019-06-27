56ab092daa5729ec0fbd37767a9afbfcdf19b6fc
rvpanos <rvpanoz@gmail.com>
2019-06-27 10:36:28 +0300

    chore: add more messages

--- app/constants/AppMessages.js
+++ app/constants/AppMessages.js
@@ -3,15 +3,26 @@ export const INFO_MESSAGES = {
   loadingPackage: 'Loading package..',
   noData: 'No dependencies found.',
   loaded: 'Package loaded.',
+<<<<<<< HEAD
   showingGlobals: 'Showing globals',
   workingDirectory: 'Working directory',
+<<<<<<< 743fc384e3be7e099f1f2cd353daafef9ae0da67
   installingPackages: 'Please wait. Installing packages..',
   updating: 'Please wait. Updating packages..',
+=======
+  installingPackages: 'Please wait. Installing packages...',
+  updating: 'Please wait. Updating packages...',
+=======
+  updating: 'Please wait. Updating packages..',
+  installing: 'Please choose installation options',
+>>>>>>> chore: add more messages
+>>>>>>> chore: add more messages
   npmInstallInfo: 'Install all packages from package.json',
   npmAuditInfo: 'Scan project for vulnerabilities',
   npmDoctorInfo: 'Run a set of checks to ensure your npm installation',
   npmAuditHelperText: 'Navigate to actions tab and run npm audit',
   npmDoctorHelperText: 'Navigate to actions tab and run npm doctor',
+<<<<<<< HEAD
   noNotifications: 'No problems',
   notGlobalModeAvailable: 'Not available in global mode',
   notificationsHelperText: 'Woohoo! There are not any problems found.',
@@ -63,6 +74,7 @@ export const TITLE_MESSAGES = {
   packageUpdate: 'Update package',
   packageUpdateLatest: 'Update to latest version',
   installLatest: 'Install latest version',
+<<<<<<< 743fc384e3be7e099f1f2cd353daafef9ae0da67
   updateSelected: 'Update selected packages',
   noNotifications: 'No problems',
   notGlobalModeAvailable: 'Not available in global mode',
@@ -94,9 +106,24 @@ export const LABEL_MESSAGE = {
   packageNameInput: 'Fill package name',
   groupType: 'Select packages based on group',
   outdatedPackages: 'Select outdated packages'
+=======
+  updateSelected: 'Update selected packages'
+=======
+  loadDirectory: 'Load a directory from a package.json file',
+  extraneous:
+    'Found extraneous packages. Run npm prune from the Tools tab to fix them.',
+  noNotifications: 'No problems',
+  notificationsHelperText: 'Woohoo! There are not any problems found.',
+  noPackages: 'No packages found',
+  noAuditData: 'No audit data',
+  noDoctorData: 'No doctor data'
+>>>>>>> chore: add more messages
+>>>>>>> chore: add more messages
 };
 
 export const CONFIRMATION_MESSAGES = {
+  actionRun:
+    'Would you like to run %actionName% \n\nNote: This process will take some time',
   installAll: `Would you like to install all the packages from \n%directory% \n\nNote: This process will take some time `,
   installPackage: 'Do you want to install %name%?',
   installLatest: 'Do you want to install %name% latest version?',
@@ -118,6 +145,8 @@ export const LABEL_MESSAGE = {
 };
 
 export const TITLE_MESSAGES = {
+  selectPackageJson: 'Select package.json file',
+  installationOptions: 'Please select installation options',
   system: 'Preview system',
   switchGlobals: 'Switch to global packages',
   showFilters: 'Show filters',
