f1fed20b749d0da0fef0ed9d7517dbea4654e64f
Arvanitis Panagiotis <panagiotis.arvanitis@agileactors.com>
2019-06-27 15:30:59 +0300

    wip: onOff operator alternative solution

--- app/models/packages/operators/onOffOperator.js
+++ app/models/packages/operators/onOffOperator.js
@@ -4,20 +4,10 @@ const onOffOperator = (on, off) => src$ => {
   const isPaused = data => [on, off].includes(data);
 
   return src$.pipe(
-    tap(data => console.log('data', data)),
-    withLatestFrom(
-      src$.pipe(
-        tap(data => console.log('inWTL', data)),
-        filter(isPaused),
-        tap(data => console.log(3, data)) // ???
-      )
-    ),
-    tap(data => console.log('wtl', data)),
+    withLatestFrom(src$.pipe(filter(isPaused))),
     filter(([value, paused]) => paused === on), // eslint-disable-line
     map(([value]) => value),
-    tap(data => console.log(1, data)),
-    filter(data => !isPaused(data)),
-    tap(data => console.log(2, data))
+    filter(data => !isPaused(data))
   );
 };
 
