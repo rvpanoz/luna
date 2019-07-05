2d7d5fd885d9696fbfac391237c410444bf5d4a3
Arvanitis Panagiotis <panagiotis.arvanitis@agileactors.com>
2019-07-05 10:53:11 +0300

    wip: lint and proptypes

--- app/components/views/packages/ToolbarActions.js
+++ app/components/views/packages/ToolbarActions.js
@@ -1,6 +1,8 @@
-import React from 'react'
-import { remote } from 'electron';
+/* eslint-disable */
 
+import React from 'react';
+import { remote } from 'electron';
+import PropTypes from 'prop-types';
 import IconButton from '@material-ui/core/IconButton';
 import Tooltip from '@material-ui/core/Tooltip';
 
@@ -15,169 +17,190 @@ import SwitchIcon from '@material-ui/icons/LoopOutlined';
 import { iMessage } from 'commons/utils';
 
 export const SwitchAction = ({ options, handler }) => {
-    const { mode } = options;
+  const { mode } = options;
 
-    return <Tooltip title={iMessage('title', 'switchGlobals')}>
-        <div>
-            <IconButton
-                disableRipple
-                disabled={mode === 'global'}
-                aria-label="show-globals"
-                onClick={() => handler('global', null)}
-            >
-                <SwitchIcon />
-            </IconButton>
-        </div>
+  return (
+    <Tooltip title={iMessage('title', 'switchGlobals')}>
+      <div>
+        <IconButton
+          disableRipple
+          disabled={mode === 'global'}
+          aria-label="show-globals"
+          onClick={() => handler('global', null)}
+        >
+          <SwitchIcon />
+        </IconButton>
+      </div>
     </Tooltip>
-}
+  );
+};
+
+SwitchAction.propTypes = {
+  handler: PropTypes.func.isRequired,
+  options: PropTypes.objectOf(PropTypes.string)
+};
 
 export const FilterAction = ({ options, handler }) => {
-    const { nodata, fromSearch } = options;
+  const { nodata, fromSearch } = options;
 
-    return <Tooltip title={iMessage('title', 'showFilters')}>
-        <div>
-            <IconButton
-                disableRipple
-                disabled={nodata || fromSearch}
-                aria-label="show-filters"
-                onClick={handler}
-            >
-                <FilterListIcon />
-            </IconButton>
-        </div>
+  return (
+    <Tooltip title={iMessage('title', 'showFilters')}>
+      <div>
+        <IconButton
+          disableRipple
+          disabled={nodata || fromSearch}
+          aria-label="show-filters"
+          onClick={handler}
+        >
+          <FilterListIcon />
+        </IconButton>
+      </div>
     </Tooltip>
-}
+  );
+};
 
 export const RefreshAction = ({ options, handler }) => {
-    const { fromSearch } = options
+  const { fromSearch } = options;
 
-    return <Tooltip title={fromSearch ? iMessage('title', 'backList') : iMessage('title', 'listReload')}>
-        <div>
-            <IconButton
-                disableRipple
-                aria-label="back-reload"
-                onClick={handler}
-            >
-                <RefreshIcon />
-            </IconButton>
-        </div>
+  return (
+    <Tooltip
+      title={
+        fromSearch
+          ? iMessage('title', 'backList')
+          : iMessage('title', 'listReload')
+      }
+    >
+      <div>
+        <IconButton disableRipple aria-label="back-reload" onClick={handler}>
+          <RefreshIcon />
+        </IconButton>
+      </div>
     </Tooltip>
-}
+  );
+};
 
-export const ClearFiltersAction = ({ handler }) => <Tooltip title={iMessage('title', 'clearFilters')}>
+export const ClearFiltersAction = ({ handler }) => (
+  <Tooltip title={iMessage('title', 'clearFilters')}>
     <IconButton
-        color="secondary"
-        aria-label="clear-filters"
-        onClick={() => handler('clearFilters')}
+      color="secondary"
+      aria-label="clear-filters"
+      onClick={() => handler('clearFilters')}
     >
-        <ClearIcon />
+      <ClearIcon />
     </IconButton>
-</Tooltip>
+  </Tooltip>
+);
 
-export const InstallAction = ({ handler }) => <Tooltip title={iMessage('title', 'installSelected')}>
+export const InstallAction = ({ handler }) => (
+  <Tooltip title={iMessage('title', 'installSelected')}>
     <IconButton
-        color="primary"
-        aria-label="install-selected"
-        onClick={() =>
-            remote.dialog.showMessageBox(
-                remote.getCurrentWindow(),
-                {
-                    title: iMessage('title', 'installSelected'),
-                    type: 'question',
-                    message: iMessage('confirmation', 'installSelected'),
-                    buttons: ['Cancel', 'Install']
-                },
-                btnIdx => {
-                    if (Boolean(btnIdx) === true) {
-                        handler('install');
-                    }
-                }
-            )
-        }
+      color="primary"
+      aria-label="install-selected"
+      onClick={() =>
+        remote.dialog.showMessageBox(
+          remote.getCurrentWindow(),
+          {
+            title: iMessage('title', 'installSelected'),
+            type: 'question',
+            message: iMessage('confirmation', 'installSelected'),
+            buttons: ['Cancel', 'Install']
+          },
+          btnIdx => {
+            if (Boolean(btnIdx) === true) {
+              handler('install');
+            }
+          }
+        )
+      }
     >
-        <AddIcon />
+      <AddIcon />
     </IconButton>
-</Tooltip>
+  </Tooltip>
+);
 
-export const LatestAction = ({ handler }) => <Tooltip title={iMessage('title', 'installLatest')}>
+export const LatestAction = ({ handler }) => (
+  <Tooltip title={iMessage('title', 'installLatest')}>
     <IconButton
-        color="primary"
-        aria-label="install-latest"
-        onClick={() =>
-            remote.dialog.showMessageBox(
-                remote.getCurrentWindow(),
-                {
-                    title: iMessage('title', 'installLatest'),
-                    type: 'question',
-                    message: iMessage('confirmation', 'installLatestSelected'),
-                    buttons: ['Cancel', 'Install']
-                },
-                btnIdx => {
-                    if (Boolean(btnIdx) === true) {
-                        handler('install', true, true);
-                    }
-                }
-            )
-        }
+      color="primary"
+      aria-label="install-latest"
+      onClick={() =>
+        remote.dialog.showMessageBox(
+          remote.getCurrentWindow(),
+          {
+            title: iMessage('title', 'installLatest'),
+            type: 'question',
+            message: iMessage('confirmation', 'installLatestSelected'),
+            buttons: ['Cancel', 'Install']
+          },
+          btnIdx => {
+            if (Boolean(btnIdx) === true) {
+              handler('install', true, true);
+            }
+          }
+        )
+      }
     >
-        <AddIcon />
+      <AddIcon />
     </IconButton>
-</Tooltip>
+  </Tooltip>
+);
 
-export const UpdateAction = ({ handler }) => <Tooltip title={iMessage('title', 'updateSelected')}>
+export const UpdateAction = ({ handler }) => (
+  <Tooltip title={iMessage('title', 'updateSelected')}>
     <IconButton
-        color="primary"
-        aria-label="update-selected"
-        onClick={() =>
-            remote.dialog.showMessageBox(
-                remote.getCurrentWindow(),
-                {
-                    title: iMessage('title', 'updateSelected'),
-                    type: 'question',
-                    message: iMessage('confirmation', 'updateSelected'),
-                    buttons: ['Cancel', 'Update']
-                },
-                btnIdx => {
-                    if (Boolean(btnIdx) === true) {
-                        handler('update');
-                    }
-                }
-            )
-        }
+      color="primary"
+      aria-label="update-selected"
+      onClick={() =>
+        remote.dialog.showMessageBox(
+          remote.getCurrentWindow(),
+          {
+            title: iMessage('title', 'updateSelected'),
+            type: 'question',
+            message: iMessage('confirmation', 'updateSelected'),
+            buttons: ['Cancel', 'Update']
+          },
+          btnIdx => {
+            if (Boolean(btnIdx) === true) {
+              handler('update');
+            }
+          }
+        )
+      }
     >
-        <UpdateIcon />
+      <UpdateIcon />
     </IconButton>
-</Tooltip>
+  </Tooltip>
+);
 
 export const UninstallAction = ({ options, handler }) => {
-    const { selected } = options
-    const hasOneSelected = selected && selected.length === 1;
-    const hasNpmSelected = selected && selected.indexOf('npm') > -1;
+  const { selected } = options;
+  const hasOneSelected = selected && selected.length === 1;
+  const hasNpmSelected = selected && selected.indexOf('npm') > -1;
 
-    return hasOneSelected && hasNpmSelected ? null : (
-        <Tooltip title={iMessage('title', 'uninstallSelected')}>
-            <IconButton
-                color="secondary"
-                aria-label="uninstall-selected"
-                onClick={() =>
-                    remote.dialog.showMessageBox(
-                        remote.getCurrentWindow(),
-                        {
-                            title: iMessage('title', 'uninstallSelected'),
-                            type: 'question',
-                            message: iMessage('confirmation', 'uninstallSelected'),
-                            buttons: ['Cancel', 'Uninstall']
-                        },
-                        btnIdx => {
-                            if (Boolean(btnIdx) === true) {
-                                handler('uninstall');
-                            }
-                        }
-                    )
-                }
-            >
-                <DeleteIcon />
-            </IconButton>
-        </Tooltip>
-    )
-}
+  return hasOneSelected && hasNpmSelected ? null : (
+    <Tooltip title={iMessage('title', 'uninstallSelected')}>
+      <IconButton
+        color="secondary"
+        aria-label="uninstall-selected"
+        onClick={() =>
+          remote.dialog.showMessageBox(
+            remote.getCurrentWindow(),
+            {
+              title: iMessage('title', 'uninstallSelected'),
+              type: 'question',
+              message: iMessage('confirmation', 'uninstallSelected'),
+              buttons: ['Cancel', 'Uninstall']
+            },
+            btnIdx => {
+              if (Boolean(btnIdx) === true) {
+                handler('uninstall');
+              }
+            }
+          )
+        }
+      >
+        <DeleteIcon />
+      </IconButton>
+    </Tooltip>
+  );
+};
