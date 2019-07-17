300988cdafb52e725b8a78f56d4d9f0ed21fe465
rvpanoz <rvpanoz@gmail.com>
2019-07-17 13:15:36 +0300

    Feat/npm audit flow design (#370)
    * wip: move Audit to containers
    
    * chore: Audit container
    
    * lint
    
    * move PackageDetails to containers, clean up, lint
    
    * chore: fix issues

--- app/components/views/audit/components/StatsCard.js
+++ app/components/views/audit/components/StatsCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { objectOf, string } from 'prop-types';
+import { objectOf, string, number } from 'prop-types';
 import { withStyles } from '@material-ui/core';
 
 import Paper from '@material-ui/core/Paper';
@@ -7,17 +7,19 @@ import Typography from '@material-ui/core/Typography';
 
 import styles from '../styles/statsCard';
 
-const StatsCard = ({ classes, title }) => (
+const StatsCard = ({ classes, title, value }) => (
   <Paper className={classes.root}>
     <div className={classes.content}>
       <Typography component="p">{title}</Typography>
+      <Typography component="h4">{value}</Typography>
     </div>
   </Paper>
 );
 
 StatsCard.propTypes = {
   classes: objectOf(string).isRequired,
-  title: string.isRequired
+  title: string.isRequired,
+  value: number
 };
 
 export default withStyles(styles)(StatsCard);
