7b82cbd8ac5e0f00d4465dfa34cd427682ddab96
rvpanoz <rvpanoz@gmail.com>
2019-07-22 01:18:29 +0300

    chore: fix import

--- app/components/views/packages/index.js
+++ app/components/views/packages/index.js
@@ -5,7 +5,6 @@ import ToolbarView from './Toolbar';
 import DialogOptionsView from './Options';
 import HeaderView from './Header';
 import PaginationView from './Pagination';
-import ActionsView from './Actions';
 
 export {
   PaginationView,
@@ -13,5 +12,4 @@ export {
   DialogOptionsView,
   ToolbarView,
   HeaderView,
-  ActionsView
 };
