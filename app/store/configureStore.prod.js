ab8a6b5e9d42274625852ec91c858509709e704e
Panagiotis.Arvanitis <Panagiotis.Arvanitis@gfk.com>
2020-09-27 22:07:11 +0300

    chore: update electron to latest version (10.1.2), rest electron packages

--- app/store/configureStore.prod.js
+++ app/store/configureStore.prod.js
@@ -2,23 +2,17 @@
  * Redux store
  */
 
-/* eslint no-underscore-dangle: 0 */
-/* eslint-disable global-require */
-
 import { createStore, compose, applyMiddleware } from 'redux';
 import { createEpicMiddleware } from 'redux-observable';
 
-// epics
 import { epics as packagesEpics } from 'models/packages';
 import { epics as uiEpics } from 'models/ui';
 import { epics as npmEpics } from 'models/npm';
 import { epics as commonEpics } from 'models/common';
 import { epics as notificationsEpics } from 'models/notifications';
-
-// reducers
 import rootReducer from '../reducers';
 
-const configureStore = initialState => {
+const configureStore = (initialState) => {
   // create epic middleware
   const epicMiddleware = createEpicMiddleware();
 
--- babel.config.js
+++ babel.config.js
@@ -10,10 +10,10 @@ const productionPlugins = [
   // babel-preset-react-optimize
   require('@babel/plugin-transform-react-constant-elements'),
   require('@babel/plugin-transform-react-inline-elements'),
-  require('babel-plugin-transform-react-remove-prop-types')
+  require('babel-plugin-transform-react-remove-prop-types'),
 ];
 
-module.exports = api => {
+module.exports = (api) => {
   // see docs about api at https://babeljs.io/docs/en/config-files#apicache
 
   const development = api.env(developmentEnvironments);
@@ -23,12 +23,10 @@ module.exports = api => {
       [
         require('@babel/preset-env'),
         {
-          targets: { electron: require('electron/package.json').version },
-          useBuiltIns: 'usage',
-          corejs: '3.0.0'
-        }
+          targets: { node: true },
+        },
       ],
-      [require('@babel/preset-react'), { development }]
+      [require('@babel/preset-react'), { development }],
     ],
     plugins: [
       // Stage 0
@@ -40,11 +38,11 @@ module.exports = api => {
       [require('@babel/plugin-proposal-optional-chaining'), { loose: false }],
       [
         require('@babel/plugin-proposal-pipeline-operator'),
-        { proposal: 'minimal' }
+        { proposal: 'minimal' },
       ],
       [
         require('@babel/plugin-proposal-nullish-coalescing-operator'),
-        { loose: false }
+        { loose: false },
       ],
       require('@babel/plugin-proposal-do-expressions'),
 
@@ -61,7 +59,7 @@ module.exports = api => {
       [require('@babel/plugin-proposal-class-properties'), { loose: true }],
       require('@babel/plugin-proposal-json-strings'),
 
-      ...(development ? developmentPlugins : productionPlugins)
-    ]
+      ...(development ? developmentPlugins : productionPlugins),
+    ],
   };
 };
